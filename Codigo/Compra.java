6d5e8ee442348e2c8db35c2dd6298941ba7ba384
Diego Tunon <data0998@criptext.com>
2020-07-16 11:28:37 -0500

    Division de las classes por archivo

---
+++ Codigo/Compra.java
@@ -0,0 +1,33 @@
+package Codigo;
+import java.util.*;
+
+
+public class Compra{
+
+    private int _idUtil;
+    private int _cantidad;
+    private int _diaCompra;
+    private int _cantDmg;
+    private Date _fechaVenc;
+
+
+    public Compra(int idUtil, int cantidad, int cantDmg, Date fechaVenc,int diaCompra){
+        this._idUtil = idUtil;
+        this._cantidad = cantidad;
+        this._cantDmg = cantDmg;
+        this._fechaVenc = fechaVenc;
+        this._diaCompra = diaCompra;
+
+    }
+
+    public int get_cantidad(){
+        return _cantidad;
+    }
+
+    public int get_diaCompra(){
+        return _diaCompra;
+    }
+
+
+
+}
---
+++ Codigo/Proveedor.java
@@ -0,0 +1,14 @@
+package Codigo;
+
+class Proveedor{
+    private String _nombre;
+
+    public Proveedor(String nombre){
+        this._nombre = nombre;
+    }
+
+    public String get_Nombre() {
+        return _nombre;
+    }
+
+}
